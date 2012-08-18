a290ac1da6f457c187f2d3d030ffd95720684735
rivolli@gmail.com <rivolli@gmail.com@41a2309a-307d-3d2f-20ee-270d7d418a97>
2012-08-18 15:35:44 +0000

    v0

---
+++ trunk/phptml5/GenericTag.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Classe Genérica para criar uma classe com o nome informado pelo usuário
+ */
+class GenericTag extends Tag {
+    private $name;
+    private $endTag;
+    
+    /**
+     * Cria uma Tag genérica
+     * 
+     * @param string $tag Nome da tag que representa o elemento
+     * @param boolean $endTag Valor lógico que define se a tag possui fechamento (TRUE) ou não (FALSE)
+     */
+    public function __construct($tag, $endTag=true){
+        $this->name = $tag;
+        $this->endTag = $endTag;
+    }
+
+    protected function getAttributeList() {
+        return array();
+    }
+
+    protected function getTagName() {
+        return $this->name;
+    }
+
+    protected function hasEndtag() {
+        return $this->endTag;
+    }
+}
---
+++ trunk/phptml5/phptml5.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+ * Include all files
+ */
+
+require_once 'Tag.php';
+require_once 'Tags.php';
+require_once 'GenericTag.php';
